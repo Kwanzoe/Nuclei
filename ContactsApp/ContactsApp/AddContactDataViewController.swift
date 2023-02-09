3cc99236201d7614ccefea0fd5d2998bbaae624c
Kwanzoe <contramol123@gmail.com>
2023-02-09 22:23:56 +0530

    Add Contact functionality added

--- ContactsApp/ContactsApp/AddContactDataViewController.swift
+++ ContactsApp/ContactsApp/AddContactDataViewController.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import Contacts
 
 class AddContactDataViewController: UIViewController {
-
+    
+    let contact = CNMutableContact()
+    let store = CNContactStore()
+    let saveRequest = CNSaveRequest()
+    
+    @IBOutlet weak var nameField: UITextField!
+        
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func addContactBtnTapped(_ sender: UIButton) {
+        if nameField.text != "" && numberField.text != ""{
+            contact.givenName  = nameField.text!
+            contact.phoneNumbers = [CNLabeledValue(
+                label: CNLabelPhoneNumberiPhone,
+                value: CNPhoneNumber(stringValue: numberField.text!))]
+            
+            saveRequest.add(contact, toContainerWithIdentifier: nil)
+            
+            
+            do {
+                try store.execute(saveRequest)
+            } catch {
+                print("Saving contact failed, error: \(error)")
+            }
+            
+        }
+        
     }
-    */
-
+    
 }
